6792c414c111ece07126e2a97e34d546ecd2972b
hwa-ryong <ocsohy@naver.com>
2023-05-16 18:19:07 +0900

    while

---
+++ ch02/src/whileexample/While2.java
@@ -0,0 +1,22 @@
+package whileexample;
+
+public class While2 {
+
+	public static void main(String[] args) {
+		// while문 - 반복조건문(if ~ break)
+		int i = 0;
+		int sumVal = 0;
+		
+		while(true) {
+			i++;
+			if(i > 10) {
+				break;
+			}
+			sumVal += i;
+			System.out.println(i);
+		}
+		System.out.println("합계: " + sumVal);
+
+	}
+
+}
