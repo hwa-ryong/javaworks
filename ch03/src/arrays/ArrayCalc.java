7f969a1e5c50eb279022f622fa95a53fa028ade8
hwa-ryong <ocsohy@naver.com>
2023-05-18 16:34:32 +0900

    copy

--- ch03/src/arrays/ArrayCalc.java
+++ ch03/src/arrays/ArrayCalc.java
@@ -6,21 +6,34 @@ public class ArrayCalc {
 		// 배열의 총점과 평균
 		// 90, 70, 85, 60 입력
 		int[] score = new int[4];
+		int total = 0;
+		double avg;
 		
-		score[0] = 90;
+		score[0] = 80;
 		score[1] = 70;
 		score[2] = 85;
 		score[3] = 60;
 		
 		// 총점 계산
-		int total = 0;
 		
-		for (int i = 0; i < score.length; i++) {
+		for(int i = 0; i < score.length; i++) {
 			total += score[i]; 
 		}
+		//평균
+		avg = (double)total / score.length;
 		
+		//최고 점수
+		int maxV = score[0]; //첫번째 값을 최고 점수로 설정
+		for(int i=0; i<score.length; i++) {
+			if(maxV < score[i]) {
+				maxV = score[i];
+			}
+		}
 		
+		//출력
 		System.out.println("총점 : " + total);
+		System.out.println("평균 : " + avg);
+		System.out.println("최고점수 : " + maxV);
 	}
 
 }
