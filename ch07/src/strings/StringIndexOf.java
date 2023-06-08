d0bd245d0a917a59678fb09cef19d60ef2962dc4
hwa-ryong <ocsohy@naver.com>
2023-06-08 11:39:43 +0900

    string , integer, system

--- ch07/src/strings/StringIndexOf.java
+++ ch07/src/strings/StringIndexOf.java
@@ -13,6 +13,15 @@ public class StringIndexOf {
 		}else {
 			System.out.println("자바와 관련이 없는 책이군요!");
 		}
+		
+		//lastIndexOf() - 찾는 문자를 뒤에서부터 검색(숫자로 변환함)
+		String uri = "/board/boardList.jsp";
+		int uri_idx1 = uri.indexOf('/');
+		
+		System.out.println(uri_idx1);  //앞에 있는 '/'
+		
+		int uri_idx2 = uri.lastIndexOf('/');
+		System.out.println(uri_idx2);  //뒤에 있는 '/'
 
 	}
 
